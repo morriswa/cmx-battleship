9862a2437610c1c6393f723af5772166b5f96594
morriswa <william@morriswa.org>
2024-08-27 07:00:02 -0500

    # dont overcomplicate things

--- src/app/components/game-tile/game-tile.component.ts
+++ src/app/components/game-tile/game-tile.component.ts
@@ -1,15 +1,18 @@
-import {Component, Input} from "@angular/core";
-import {GameTileDirective} from "../../directives/game-tile.directive";
+import {Component, HostListener, Input} from "@angular/core";
 
 @Component({
   selector: 'app-game-tile',
   templateUrl: './game-tile.component.html',
   standalone: true,
-  imports: [
-    GameTileDirective
-  ],
+  imports: [],
   styleUrl: "./game-tile.component.scss"
 })
 export class GameTileComponent {
   @Input() tileId!: string;
+
+  // detect mousedown events on game-tile
+  @HostListener('mousedown') onClick() {
+    // execute every click
+    console.log(`click detected ${this.tileId}`);
+  }
 }
