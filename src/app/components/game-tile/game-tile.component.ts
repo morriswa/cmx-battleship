44766b7ff0d9c088043a7a3cb79a79ebe52b7f67
morriswa <william@morriswa.org>
2024-08-26 20:32:40 -0500

    + game tile directive detect mousedown event

--- src/app/components/game-tile/game-tile.component.ts
+++ src/app/components/game-tile/game-tile.component.ts
@@ -1,9 +1,13 @@
 import {Component, Input} from "@angular/core";
+import {GameTileDirective} from "../../directives/game-tile.directive";
 
 @Component({
   selector: 'app-game-tile',
   templateUrl: './game-tile.component.html',
   standalone: true,
+  imports: [
+    GameTileDirective
+  ],
   styleUrl: "./game-tile.component.scss"
 })
 export class GameTileComponent {
