111c047af7d45afe1594db6866cb62072a6c6b77
makennabrynn <makennaloewenherz@gmail.com>
2024-08-27 15:19:19 -0500

    added landing page

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,9 +1,14 @@
 import {Routes} from "@angular/router";
 import {PlayGameComponent} from "./pages/play-game/play-game.component";
 import {AboutUsComponent} from "./pages/about-us/about-us.component";
+import { LandingPageComponent } from "./pages/landing-page/landing-page.component";
 
 
 export const routes: Routes = [
+  {
+    path: "start",
+    component: LandingPageComponent,
+  },
   {
     path: "play",
     component: PlayGameComponent,
