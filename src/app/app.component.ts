71cfe365be1d84618ed8fbb0913058e582b01153
morriswa <william@morriswa.org>
2024-09-09 14:28:53 -0500

    finish mobile mask

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,13 @@
-import {Component, HostListener, inject, OnDestroy, OnInit, signal, WritableSignal} from "@angular/core";
+import {
+  AfterViewInit,
+  Component, ElementRef,
+  HostListener,
+  inject,
+  OnDestroy,
+  OnInit,
+  signal,
+  WritableSignal
+} from "@angular/core";
 import {
   NavigationCancel,
   NavigationEnd,
@@ -29,12 +38,17 @@ import {MobileDisabledMaskComponent} from "./components/mobile-disabled-mask/mob
     MobileDisabledMaskComponent
   ]
 })
-export class AppComponent implements OnInit, OnDestroy{
+export class AppComponent implements OnInit, AfterViewInit, OnDestroy {
 
+  private minWindowHeightPx?: number;
+  private minWindowWidthPx?: number;
+
+  private host: ElementRef = inject(ElementRef);
   private router = inject(Router);
   private _routerEventSubscription?: Subscription;
+
   protected loading: WritableSignal<boolean> = signal(false);
-  protected mobile: WritableSignal<boolean> = signal(window.innerWidth<600);
+  protected mobile: WritableSignal<boolean> = signal(false);
 
   ngOnInit() {
     this._routerEventSubscription = this.router.events.subscribe((event) => {
@@ -50,12 +64,31 @@ export class AppComponent implements OnInit, OnDestroy{
     });
   }
 
+  ngAfterViewInit() {
+
+    const scssValY = window.getComputedStyle(this.host.nativeElement).getPropertyValue('--app-min-window-height');
+    this.minWindowHeightPx = Number(scssValY.substring(0, scssValY.indexOf('p')))
+
+    const scssValX = window.getComputedStyle(this.host.nativeElement).getPropertyValue('--app-min-window-width')
+    this.minWindowWidthPx = Number(scssValX.substring(0, scssValX.indexOf('p')))
+
+    this.refreshMobileSignal();
+  }
+
   ngOnDestroy() {
     this._routerEventSubscription?.unsubscribe();
   }
 
   @HostListener('window:resize', ['$event'])
+  windowResize() {
+    this.refreshMobileSignal();
+  }
+
   refreshMobileSignal() {
-    this.mobile.set(window.innerWidth<600);
+    if (!this.minWindowHeightPx || !this.minWindowWidthPx) {
+      this.mobile.set(false);
+    } else {
+      this.mobile.set(window.innerWidth<this.minWindowWidthPx || window.innerHeight<this.minWindowHeightPx)
+    }
   }
 }
