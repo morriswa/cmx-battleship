c4a04b7672b99452131e1de98b951f2aaca30dbc
morriswa <william@morriswa.org>
2024-09-13 16:44:57 -0500

    while loading only display background

--- src/app/components/fullscreen-load-mask/fullscreen-load-mask.component.ts
+++ src/app/components/fullscreen-load-mask/fullscreen-load-mask.component.ts
@@ -1,9 +1,13 @@
 import {Component} from "@angular/core";
+import {CloudBackgroundComponent} from "../cloud-background/cloud-background.component";
 
 @Component({
   selector: "app-fullscreen-load-mask",
   templateUrl: "./fullscreen-load-mask.component.html",
   styleUrl: "./fullscreen-load-mask.component.scss",
+  imports: [
+    CloudBackgroundComponent
+  ],
   standalone: true
 })
 export class FullscreenLoadMaskComponent {
