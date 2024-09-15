d63d14c6736727b146493b91574b71df96ae8551
morriswa <william@morriswa.org>
2024-09-15 11:18:44 -0500

    Kevin Rivers made comments for files.

--- src/app/components/cloud-background/cloud-background.component.ts
+++ src/app/components/cloud-background/cloud-background.component.ts
@@ -1,6 +1,10 @@
 import {Component, Input} from "@angular/core";
 import {NgOptimizedImage} from "@angular/common";
 
+/**
+ * This component is used to animate the cloud background on the https://www.morriswa.org/cmx/battleship/start webpage.
+ * There are two different types of backgrounds. One is cloud which combines 3 images, and partly-cloud that combines 2 images.
+ */
 @Component({
   selector: "app-cloud-background",
   templateUrl: "./cloud-background.component.html",
--- src/app/types/lobby.types.ts
+++ src/app/types/lobby.types.ts
@@ -9,6 +9,7 @@ export type NewUserSession = {
   player_id: string;
 }
 
+//each session has a session id, player id, name, number of ships
 export type UserSession = {
   session_id: string;
   player_id: string;
@@ -16,15 +17,18 @@ export type UserSession = {
   num_ships: string;
 }
 
+//Displays the number of players the game.
 export type OnlineStats = {
   playerCount: number;
 }
 
+//Players not currently in a match.
 export type AvailablePlayer = {
   player_id: string;
   player_name: string;
 }
 
+//
 export type GameRequest = {
   game_request_id: string;
   player_id: string;
