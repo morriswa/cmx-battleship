0bbbfa27aefc22a14b397c6b82dc036780d69a5a
morriswa <william@morriswa.org>
2024-09-15 11:59:34 -0500

    Adding comments, KR

--- src/app/components/mobile-disabled-mask/mobile-disabled-mask.component.ts
+++ src/app/components/mobile-disabled-mask/mobile-disabled-mask.component.ts
@@ -1,7 +1,8 @@
 import {Component} from "@angular/core";
 
 /*
-  This component displays to the user that the mobile implementation
+  This component displays to the user that the mobile implementation of our project is not set up.
+  This typically occurs at 200% zoom.
 */
 
 @Component({
